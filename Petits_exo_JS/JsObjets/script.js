e4e397754971f5e95f2a918da4d5be4bd2893327
kiingsora <hichem.m83@gmail.com>
2025-08-05 15:11:51 +0200

    Dossier Js Objet script.js : création d'un objet imbriqué + comment les selectionner

--- Petits_exo_JS/JsObjets/script.js
+++ Petits_exo_JS/JsObjets/script.js
@@ -7,11 +7,11 @@ const jedi = {
   age:29,
   cote: 'obscur'
 }
+
 // affichage des propriété de l'objet
 console.log(jedi.prenom, jedi.nom, jedi.age);
 // affichage de l'objet
 console.log(jedi); 
-
 
 // Niveau 2 exo 2 changer et ajouter un objet
 jedi.prenom = "anakin";
@@ -20,7 +20,6 @@ jedi.couleurDeSabre = "rouge";
 console.log(jedi);
 
 // niveau 3 exo 3 ajouter et supprimer une propriété/champ
-
 // ajout d'un champ 
 jedi.statut = "élu de la force"
 console.log(`ajout d'un champ`,jedi);
@@ -35,10 +34,9 @@ for (const propriete in jedi) {
 }
 
 // niveau 5 exo 5 Méthodes et this
-
 // ajout d'une fonction dans l'objet jedi qui affiche un message de présentation
 jedi.sePresenter = function(){
-  let message = `Je m'appelle ${this.prenom} ${this.nom}...`;
+  let message = `Je suis ${this.prenom} ${this.nom}...`;
 
 // condition selon le côté de la force
   if (this.cote === "obscur") {
@@ -47,10 +45,11 @@ jedi.sePresenter = function(){
     message += 'Rejoins le côté lumineux de la force'
   };
   console.log(message);
-  
 }
+
 // appel de la fonction sePrésenter d l'objet jedi
 jedi.sePresenter();
+console.log(jedi);
 
 // créer une méthode avec une fonction fléché (la méthode est lié à l'objet donc this n'est pas utilisable dans ce cas)
 const droid = {
@@ -63,3 +62,40 @@ const droid = {
 }
 
 console.log(droid.parler());
+
+// Niveau 6 exo 6 : on récapitule tout 
+// 
+const sorciers = {
+  prenom: "Harry",
+  nom : "Potter",
+  age:15,
+  ecole: "Poudlard",
+  sortAppris: ["Expeliarmus", "Wingardium Leviosa", "Protego"],
+  maison : "Griffondor"
+}
+
+for (const attribut in sorciers) {
+ console.log(attribut, sorciers[attribut]);
+}
+sorciers.afficherProfil = function () {
+  let message = `étudiant : ${this.maison}`
+  console.log(message);
+}
+delete sorciers.age;
+console.log(sorciers);
+
+
+// OBJET IMBRIQUé
+const personnageDeJeu =
+  {
+    prenom: "sora",
+    equipement: {
+      arme: "Keyblade",
+      armure: "vêtements magique"
+    },
+    competence: ["maniement de la keyblade", "Sort magique", "invocation","Joie de vivre"],
+  };
+
+console.log(personnageDeJeu.equipement.arme);
+console.log(personnageDeJeu.competence[0]);
+
