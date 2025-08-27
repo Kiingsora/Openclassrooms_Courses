4344d9ca5190d3366d9fa896f251bf80a1f31f27
kiingsora <hichem.m83@gmail.com>
2025-08-27 12:50:53 +0200

    Dossier Js : Les classes : exo 1 : les prototypes + test

--- Petits_exo_JS/JsClass/script.js
+++ Petits_exo_JS/JsClass/script.js
@@ -15,11 +15,24 @@ class Personne {
     return mot[0].toUpperCase() + mot.slice(1); 
   }
   presentation() {
-    const phrase = `je m'appelle ${this.toCapitalizeAndVerificationTrimOrEmptyValue(this.nom)} ${this.toCapitalizeAndVerificationTrimOrEmptyValue(this.prenom)}, j'ai ${this.age} ans`;
+    const phrase = this.toCapitalizeAndVerificationTrimOrEmptyValue(`je m'appelle ${this.toCapitalizeAndVerificationTrimOrEmptyValue(this.nom)} ${this.toCapitalizeAndVerificationTrimOrEmptyValue(this.prenom)}, j'ai ${this.age} ans`);
     return phrase
   }
 }
-const malik = new Personne("malik", "elif", 28);
+
+// créer une nouvelle instance
+const malik = new Personne("elif", "malik", 28);
+// créer un objet vide qui emprunte les méthodes quand il a besoin
+const ahmed = Object.create(malik)
+ahmed.prenom = "ahmed";
+// test de la méthode présentation
+ahmed.presentation()
+// créer une instance karen a partir de malik et valeur direct dans le constructeur 
+const karen = new malik.constructor("karen", 'matu', 85)
+// vérification 
+console.log(ahmed.presentation()); // renvoi la classe personne
+console.log(malik.constructor.name); // renvoi le nom de la classe
+console.log(karen.age); //affiche l'age de l'instance de karen
 
 
 
