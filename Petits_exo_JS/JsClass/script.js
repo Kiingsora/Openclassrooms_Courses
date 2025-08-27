9fff5af200bb616c159ffea0afbf70c6b5be231e
kiingsora <hichem.m83@gmail.com>
2025-08-27 12:10:38 +0200

    Dossier Js : Les classes : exo 1 : créer une classe + méthodes (maj)

--- Petits_exo_JS/JsClass/script.js
+++ Petits_exo_JS/JsClass/script.js
@@ -4,34 +4,84 @@ class Personne {
     this.prenom = prenom;
     this.age = age;
   }
-
-  capitalize(phraseACapitaliser) {
-    const phrase = phraseACapitaliser[0].toUpperCase() + phraseACapitaliser.slice(1);
-    return phrase;
-  }
-
-  presentation() {
-    
-    const phrase = this.capitalize(`je m'appelle ${this.nom} ${this.prenom} et j'ai ${this.age} ans`);
-
-    if (isNaN(this.age)) {
-      console.log(`Entrez un numéro dans la case age de ${this.prenom}`);
-    } else {
-      return phrase;
+  toCapitalizeAndVerificationTrimOrEmptyValue(mot){
+    if (typeof mot !== "string") {
+      return ""
     }
+    mot = mot.trim();
+    if (mot === "") {
+      return ""
+    }
+    return mot[0].toUpperCase() + mot.slice(1); 
   }
-
-  affichage() {
-    console.log(this.presentation());
+  presentation() {
+    const phrase = `je m'appelle ${this.toCapitalizeAndVerificationTrimOrEmptyValue(this.nom)} ${this.toCapitalizeAndVerificationTrimOrEmptyValue(this.prenom)}, j'ai ${this.age} ans`;
+    return phrase
   }
 }
+const malik = new Personne("malik", "elif", 28);
 
-const personnes = [
-  new Personne("Dumont", "hervé", "gjgf"),
-  new Personne("hiyaa", "Alice", 23),
-  new Personne("Dumont", "julien", 33),
-];
 
-personnes.forEach((perso) => {
-  perso.affichage();
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // capitalize(phraseACapitaliser) {
+  //   const phrase =
+  //     phraseACapitaliser[0].toUpperCase() + phraseACapitaliser.slice(1);
+  //   return phrase.trim();
+  // }
+
+//   presentation() {
+//     const phrase = this.capitalize(
+//       `je m'appelle ${this.nom} ${this.prenom} et j'ai ${this.age} ans`
+//     );
+//     if (
+//       isNaN(this.age) ||
+//       typeof this.nom !== "string" ||
+//       typeof this.prenom !== "string" ||
+//       this.nom === "" ||
+//       this.prenom === "" ||
+//       this.age === "" 
+//     ) {
+//       console.log(
+//         `Entrez du texte dans les cases nom et prénom et un chiffre pour la case age`
+//       );
+//       return;
+//     } else {
+//       return phrase;
+//     }
+//   }
+
+//   affichage() {
+//     console.log(this.presentation());
+//   }
+// }
+
+// const personnes = [
+//   new Personne("Dumont", "hervé", 33),
+//   new Personne(54651, "", 66),
+//   new Personne("Dumont", "julien", 33),
+// ];
+
+// personnes.forEach((perso) => {
+//   perso.affichage();
+// });
